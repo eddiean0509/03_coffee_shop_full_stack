2fa365234b791afa38f549c780a3675e2b3c74a7
KK <dukhoon.kang@email.com>
2023-03-02 23:15:00 +0900

    test 샘플

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -6,16 +6,27 @@ from flask_sqlalchemy import SQLAlchemy
 from flaskr import create_app
 from models import setup_db, Question, Category
 
+DATABASE_NAME = "trivia_test"
+DATABASE_PATH = "postgresql://postgres:postgres@127.0.0.1:5432/trivia_test"
+
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        # load test data
+        os.system(f'psql "{DATABASE_PATH}" {DATABASE_NAME} -c "DROP TABLE questions, categories;"')
+        os.system(f'psql "{DATABASE_PATH}" {DATABASE_NAME} < ./trivia.psql')
 
     def setUp(self):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgresql://{}/{}".format('postgres:postgres@127.0.0.1:5432', self.database_name)
+        self.database_name = DATABASE_NAME
+        self.database_path = DATABASE_PATH
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,6 +44,37 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+
+    def test_available_categories(self):
+        response = self.client().get('/categories')
+        self.assertEqual(response.status_code, 200)
+
+        data = json.loads(response.data)
+        self.assertEqual(len(data["categories"]), 6)
+        self.assertEqual(data["categories"]["4"], "History")
+
+    def test_list_questions(self):
+        response = self.client().get('/questions')
+        self.assertEqual(response.status_code, 200)
+
+        data = json.loads(response.data)
+        self.assertEqual(len(data["questions"]), 10)
+        self.assertEqual(data["total_questions"], 19)
+
+    def test_delete_question(self):
+        pass
+
+    def test_create_question(self):
+        pass
+
+    def test_search_questions(self):
+        pass
+
+    def test_category_questions(self):
+        pass
+
+    def test_quizzes(self):
+        pass
 
 
 # Make the tests conveniently executable
